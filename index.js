c22ead2a7446a7b443a47cb81779f67bd6b681a4
chandrawardhana01 <ugmbackend01@gmail.com>
2020-10-13 09:18:46 +0700

    commit import upload file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
-import bodyParcel from 'body-parser'
-
+import bodyParser from 'body-parser'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 //const database = require('./database')
 import { initDatabase, initTable, insertProduct, getProduct } from './database.js'
 
@@ -17,29 +20,55 @@ app.set('views', __dirname + '/layout')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+app.use(fileUpload())
+
 app.use(morgan('combined'))
 
-app.use(bodyParcel.urlencoded())
+app.use(bodyParser.urlencoded())
 
 app.use('/assets', express.static(__dirname + '/assets'))
-
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
     res.send({ success: true})
 })
 
-app.get('/product', (req, res, next) => {
-    res.render('product')
-})
+app.get('/product', async (req, res, next) => {
+  
+    let products
+    try {
+      products = await getProduct(db)
+    } catch (error) {
+      return next(error)
+    }
+  
+    res.render('product', { products })
+  })
 
 app.get('/add-product', (req, res, next) => {
     res.send(req.query)
 })
 
-app.post('/add-product', (req, res, next)=> {
+app.post('/add-product', (req, res, next) => {
     console.log('Request', req.body)
-    res.send(req.body)
-})
+    console.log('File', req.files)
+    // get file name
+    const fileName = Date.now() + req.files.photo.name
+  
+    // write file
+    fs.writeFile(path.join(__dirname, '/files/', fileName), req.files.photo.data, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+  
+      // insert product
+      insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
+      
+      // redirect
+      res.redirect('/product')
+    })
+  })
 
 app.use((err, req, res, next) =>{
     res.send(err.message)
